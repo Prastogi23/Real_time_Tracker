8e132dc2e212c9ecab54cd0d835d4dba92a7c647
Prakhar Rastogi <115170581+Prastogi23@users.noreply.github.com>
2024-12-20 19:13:30 +0530

    Real_time_Tracker
    Complete module  of real time tracker

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+const socket = io();
+
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location" ,{latitude , longitude});
+        },
+        (error) =>{
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 500 ,
+            maximumAge: 0 ,
+        }
+    );
+}
+
+const map = L.map("map").setView([0,0] , 20);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" , {
+    attribution : "Prakhar Rastogi",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location" , function (data){
+    const {id , latitude , longitude} = data;
+    map.setView([latitude,longitude]);
+    if(markers[id])
+        {
+        markers[id].setLatLng([latitude,longitude]);
+    }else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+        console.log(markers);
+    }
+});
+
+socket.on("user-disconnected" , (id)=> {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
+
